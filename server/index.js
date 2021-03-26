e7a79cfb2245df313a03c7fb60c2d90bafb309e1
SeoBoSeok <ggy7bbo@gmail.com>
2021-03-27 06:22:16 +0900

    add more function for looking serverlike

--- server/index.js
+++ server/index.js
@@ -1,6 +1,47 @@
 const http = require('http');
 const { URL } = require('url');
 const fs = require('fs');
+const { logger } = require('./config/winston');
+
+let childProcess = require('child_process');
+var cron = require('node-cron');
+
+/**
+ * https://www.npmjs.com/package/node-cron
+ */
+cron.schedule('* * * * *', () => {
+  console.log('running a task every minute');
+  logger.info('SCHEDULE crawler');
+  runScript(__dirname + '/crawler-script.js', function (err) {
+    if (err) throw err;
+    console.log('finished running crawler-script.js');
+  });
+});
+
+function runScript(scriptPath, callback) {
+
+    // keep track of whether callback has been invoked to prevent multiple invocations
+    let invoked = false;
+
+    let process = childProcess.fork(scriptPath);
+
+    // listen for errors as they may prevent the exit event from firing
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err);
+    });
+
+    // execute the callback once the process has finished running
+    process.on('exit', function (code) {
+        if (invoked) return;
+        invoked = true;
+        var err = code === 0 ? null : new Error('exit code ' + code);
+        callback(err);
+    });
+
+}
+
 http.createServer((request, response) => {
   const baseURL = 'http://' + request.headers.host + '/';
   const myURL = new URL(request.url, baseURL);
@@ -14,7 +55,16 @@ http.createServer((request, response) => {
         }
         response.end(data, 'utf-8'); // 브라우저로 전송
       });
-    } else if (path === '/') { // 주소가 /이면
+    } else if (path === '/crawler') {
+      logger.info(`GET crawler `);
+      // Now we can run a script and invoke a callback when complete, e.g.
+      runScript(__dirname + '/crawler-script.js', function (err) {
+        if (err) throw err;
+        console.log('finished running crawler-script.js');
+        response.end('crawler worked.');
+      });
+    }
+     else if (path === '/') { // 주소가 /이면
       response.writeHead(200,{'Content-Type':'text/html'});
       fs.readFile(__dirname + '/main.html', (err, data) => {
         if (err) {
@@ -23,6 +73,8 @@ http.createServer((request, response) => {
         response.end(data, 'utf-8');
       });
     } else { // 매칭되는 주소가 없으면
+      response.writeHead(404,{'Content-Type':'text/html;charset=UTF-8'});
+      logger.error(`Error message: 잘못된 주소 참조. ${path}`);
       response.statusCode = 404; // 404 상태 코드
       response.end('주소가 없습니다');
     }
